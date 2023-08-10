73cb3955778223ba6e07ad5153f57350250ccab3
Geparrrrda <61092061+Geparrrrda@users.noreply.github.com>
2023-08-11 02:36:27 +0300

    Add files via upload

---
+++ calculator.cs
@@ -0,0 +1,54 @@
+using System; 
+using System.Linq; 
+using System.Collections.Generic;
+using CSharpShellCore; 
+namespace HelloWold; 
+public static class Program 
+{ 
+public static void Main() 
+{
+		float oneNum;
+		float twoNum;
+		float result;
+		
+		string oper;
+		
+		Console.Write("Write the first number(The fraction must be written with \",\"): ");
+		oneNum = Convert.ToSingle(Console.ReadLine());
+		
+		Console.Write("Write an operator(+,-,*,/): ");
+		oper = Console.ReadLine();
+		
+		Console.Write("Write the second number(The fraction must be written with \",\"): ");
+		twoNum = Convert.ToSingle(Console.ReadLine());
+		
+		if (oper == "+")
+		{
+			result = oneNum + twoNum;
+			Console.WriteLine(result);
+		}
+		
+		else if (oper == "-")
+		{
+			result = oneNum - twoNum;
+			Console.WriteLine(result);
+		}
+		
+		else if (oper == "*")
+		{
+			result = oneNum * twoNum;
+			Console.WriteLine(result);
+		}
+		
+		else if (oper == "/")
+		{
+			result = oneNum / twoNum;
+			Console.WriteLine(result);
+		}
+		else
+		{
+			Console.WriteLine("Sorry, but you entered the wrong operator");
+		}
+		 
+} 
+} 
